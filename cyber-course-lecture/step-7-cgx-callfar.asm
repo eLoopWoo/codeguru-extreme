368fffa582f2b0354d78fad2b8d6897c2b41be85
Tomer Eyzenberg <eyzenberg.tomer@gmail.com>
2017-11-14 23:36:18 +0200

    modified: cyber-course-lecture/step-7-cgx-callfar.asm

--- cyber-course-lecture/step-7-cgx-callfar.asm
+++ cyber-course-lecture/step-7-cgx-callfar.asm
@@ -1,17 +1,3 @@
-call address
-	ss:sp = word (ip+3)
-	ip = word (ip + address + 0)
-	sp -= 2
-
-call far register
-	ip = word (register + 0)
-	cs = word (register + 2)
-	ss:sp = dword cs:(ip+2)
-	
-	sp -= 4
-
-;callfar;
-
 call get_ip
 get_ip:
 pop bx
@@ -26,4 +12,5 @@ push ds
 pop ss
 mov word [si + 2],bx
 mov sp,ax
-callfar_:call far [si]
+callfar_:
+call far [si]
