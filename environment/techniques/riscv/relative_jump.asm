38bec9ee6154fd269f19a0187bfbbe09ab3e0cc0
eLoopWoo <eyzenberg.tomer@gmail.com>
2019-01-07 19:44:14 -0500

    add(techniques/riscv/scout_low)

--- environment/techniques/riscv/relative_jump.asm
+++
@@ -1,6 +0,0 @@
-# Technique - Relative Jump
-add x2, x1, 50
-looper:
-	sw x1, (x2)
-	add x2, x2, 4
-	j looper
---
+++ environment/techniques/riscv/scout_low.asm
@@ -0,0 +1,25 @@
+# Technique - Scout ( Low Address )
+ 
+set_scout:
+    li x5, 0   
+    sw x1, -74(x1)
+    li x4, 10
+    mv x6, x1 
+
+scout:
+    lw x3, -74(x1)
+    bne x3,x1,scout_low_addr
+    j scout
+
+scout_low_addr:
+    mv x6, x1
+    loop:
+        add x4, x4, -1
+        lw x2, (x1)
+        sw x2, -74(x1)
+        add x1, x1, 4
+        bne x4, x5, loop
+    mv x1, x6
+    add x1, x1, -74
+    j x1-74
+
